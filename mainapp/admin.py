7585406c20b2eabadb5f327608cc8ae5ebf6f009
Rudrappa-Naidu <rudrappa94922@gmail.com>
2021-11-29 12:17:23 +0530

    model Employee Departments

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from mainapp.models import Employees, Departments
+
+admin.site.register(Employees)
+admin.site.register(Departments)
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,20 @@
+import to as to
 from django.db import models
 
 # Create your models here.
+class Employees(models.Model):
+    first_name=models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    contact_number = models.IntegerField(max_length=100,unique=True)
+    employee_id=models.IntegerField(max_length=100,unique=True,primary_key=True)
+    email = models.EmailField(max_length=100)
+
+
+class Departments(models.Model):
+    Employee=models.ForeignKey(Employees,on_delete=models.CASCADE)
+    software=models.CharField(max_length=100)
+    Marketing=models.CharField(max_length=100)
+    sales=models.IntegerField(max_length=100)
+    Design=models.CharField(max_length=100)
+    production=models.CharField(max_length=100)
+
